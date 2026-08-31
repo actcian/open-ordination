3aefa8ac4c8a229cd9cb92855d3ff9b082dc4966
actcian <175528868+actcian@users.noreply.github.com>
2026-08-31 17:51:00 +0700

    Add repeatable Duo-style practice

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='open-ordination-v6';
+const CACHE='open-ordination-v7';
 const ASSETS=['./','./index.html','./trainer.html','./manifest.webmanifest','./favicon.svg'];
 
 self.addEventListener('install',event=>{
--- test.js
+++ test.js
@@ -5,4 +5,6 @@ const listen=html.match(/<main id="listen"[\s\S]*?<main id="test"/)[0];
 assert(listen.includes('PLXRewNkdAh9XyUiNo8xRt365g09gb-nkz'));
 assert(listen.includes('./trainer.html'));
 assert(!listen.includes('onclick="speak('));
+assert(html.includes('<main id="duo"'));
+assert(html.includes('function resetDuo()'));
 console.log('listen tab uses the YouTube playlist');
