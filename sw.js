22198c256f24c56036c80f625fda9d9560f81466
actcian <175528868+actcian@users.noreply.github.com>
2026-09-03 16:01:15 +0700

    Add targeted difficult-line review

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='open-ordination-v20';
+const CACHE='open-ordination-v21';
 const ASSETS=['./','./index.html','./trainer.html','./manifest.webmanifest','./favicon.svg','./original-2807.jpg','./original-2808.jpg','./original-2809.jpg'];
 
 self.addEventListener('install',event=>{
