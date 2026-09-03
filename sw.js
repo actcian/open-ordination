0bbdd1a9b0572e5884cc5764e7c2fc9df7746a86
actcian <175528868+actcian@users.noreply.github.com>
2026-09-03 13:55:16 +0700

    Split chant practice into lesson sets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='open-ordination-v19';
+const CACHE='open-ordination-v20';
 const ASSETS=['./','./index.html','./trainer.html','./manifest.webmanifest','./favicon.svg','./original-2807.jpg','./original-2808.jpg','./original-2809.jpg'];
 
 self.addEventListener('install',event=>{
