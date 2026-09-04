aac8871122e42536395c6199c72c5b3e6ba58575
actcian <175528868+actcian@users.noreply.github.com>
2026-09-04 18:17:17 +0700

    Read Gemini transcription response

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='open-ordination-v27';
+const CACHE='open-ordination-v28';
 const ASSETS=['./','./index.html','./trainer.html','./manifest.webmanifest','./favicon.svg','./original-2807.jpg','./original-2808.jpg','./original-2809.jpg'];
 
 self.addEventListener('install',event=>{
